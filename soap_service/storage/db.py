0b94aeec92b29ec1ae2a5addb6e6386fdde00cfa
Igor Pestov <pestov.igr@gmail.com>
2020-02-19 21:41:53 +0500

    feat: Добаление и получение информации о Stock

--- soap_service/storage/db.py
+++ soap_service/storage/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, Column, Float, Integer, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_PATH = os.environ.get('DB_PATH', '')
+DB_PATH = os.environ.get('DB_PATH', os.path.dirname(os.path.realpath(__file__)))
 DB_NAME = os.environ.get('DB_NAME', 'app.db')
 
 Base = declarative_base()
@@ -38,6 +38,29 @@ class User(Base):
     def __str__(self):
         return f'<User: {self.name}>'
 
+
 def init_db():
     engine = _sqlite_engine()
     Base.metadata.create_all(engine)
+
+
+def _get_session():
+    engine = _sqlite_engine()
+    session = sessionmaker()
+    session.configure(bind=engine)
+    return session()
+
+
+def stock_create(name, price):
+    session = _get_session()
+    stock = Stock(name=name, price=price)
+    session.add(stock)
+    session.commit()
+
+
+def stock_price_by_name(name):
+    session = _get_session()
+    stock = session.query(Stock).filter_by(name=name).first()
+    if stock:
+        return stock.price
+    return None
