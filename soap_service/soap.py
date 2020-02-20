012e59024c2882c8a06ae2e45c7a26d2bee9a4c1
Igor Pestov <pestov.igr@gmail.com>
2020-02-20 16:43:47 +0500

    feat: Базовая аутентификация пользователя

--- soap_service/soap.py
+++ soap_service/soap.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from spyne import rpc, ServiceBase, Integer, Unicode, Float
+from spyne import rpc, ServiceBase, Integer, Unicode, Float, ComplexModel, String
 from spyne.model.fault import Fault
 from soap_service import handlers
+
+
+class AuthHeader(ComplexModel):
+    UserName = Unicode
+    Session = Unicode
 
 
 class StockService(ServiceBase):
@@ -20,3 +25,18 @@ class StockService(ServiceBase):
         if result:
             raise Fault('Client', 'Невозможно изменить запись. Запись не найдена')
         return result
+
+
+class UserService(ServiceBase):
+
+    @rpc(Unicode, Unicode, _operation_name='Login', _returns=Unicode)
+    def login(ctx, User, Password):
+        return ''
+
+    @rpc(Unicode, Unicode, _operation_name='Register', _returns=String)
+    def register(ctx, UserName, Password):
+        return ''
+
+    @rpc(Unicode, _operation_name='ChangePassword', _returns=String, _in_header=(AuthHeader,))
+    def change_password(ctx, Password):
+        return ''
